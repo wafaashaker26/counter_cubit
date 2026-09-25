607931e2d3b5d3d2449d6c6c648bdc8822910cb0
wafaashaker26 <wafaashaker@students.du.edu.eg>
2026-09-25 17:34:54 +0300

    add shared_preferences

--- lib/feature/cubit/counter_cubit.dart
+++ lib/feature/cubit/counter_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 part 'counter_state.dart';
 
@@ -8,12 +9,26 @@ class CounterCubit extends Cubit<CounterState> {
 
   int scoreA = 0, scoreB = 0;
 
+  Future<void> loadData()async{
+    final shared =await SharedPreferences.getInstance();
+    scoreA=shared.getInt('scoreA')??0;
+    scoreB=shared.getInt('scoreB')??0;
+    emit(CounterIncreaseScore());
+  }
+
+  Future<void>save()async{
+    final prefs=await SharedPreferences.getInstance();
+    prefs.setInt("scoreA", scoreA);
+    prefs.setInt("scoreB", scoreB);
+  }
+
   void increaseScore({required String nameTeam, required int point}) {
     if (nameTeam == 'A') {
       scoreA += point;
     } else {
       scoreB += point;
     }
+    save();
     emit(CounterIncreaseScore());
   }
 
