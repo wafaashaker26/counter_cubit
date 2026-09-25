38ad8ba768dcc7878b943a1a6f9b3c15329b4929
wafaashaker26 <wafaashaker@students.du.edu.eg>
2026-09-25 17:22:09 +0300

    add BlocProvider

--- lib/feature/counter_screen.dart
+++ lib/feature/counter_screen.dart
@@ -1,6 +1,8 @@
+import 'package:basketball_counter/feature/cubit/counter_cubit.dart';
 import 'package:basketball_counter/feature/widgets/button_widget.dart';
 import 'package:basketball_counter/feature/widgets/column_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
@@ -10,37 +12,44 @@ class CounterScreen extends StatefulWidget {
 }
 
 class _CounterScreenState extends State<CounterScreen> {
-  int teamAScore = 0;
-  int teamBScore = 0;
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Points Counter", style: TextStyle(color: Colors.white)),
-        backgroundColor: Color(0xffff9900),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          spacing: 50,
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                ColumnWidget(teamName: "A"),
-                Container(
-                  height: MediaQuery.of(context).size.height * .5,
-                  width: 2,
-                  color: Colors.black,
-                ),
-                ColumnWidget(teamName: "B"),
-              ],
+    return BlocProvider(
+      create: (context) => CounterCubit(),
+      child: BlocBuilder<CounterCubit, CounterState>(
+        builder: (context, state) {
+          final cubit=BlocProvider.of<CounterCubit>(context);
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("Points Counter", style: TextStyle(color: Colors.white)),
+              backgroundColor: Color(0xffff9900),
             ),
-            ButtonWidget(title: "Reset", onPressed: () {}),
-          ],
-        ),
+            body: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                spacing: 50,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ColumnWidget(nameTeam: "A"),
+                      Container(
+                        height: MediaQuery.of(context).size.height * .5,
+                        width: 2,
+                        color: Colors.black,
+                      ),
+                      ColumnWidget(nameTeam: "B"),
+                    ],
+                  ),
+                  ButtonWidget(title: "Reset", onPressed: () {
+                    cubit.resetScore();
+                  }),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
--- lib/feature/widgets/column_widget.dart
+++ lib/feature/widgets/column_widget.dart
@@ -1,32 +1,30 @@
+import 'package:basketball_counter/feature/cubit/counter_cubit.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'button_widget.dart';
 
 class ColumnWidget extends StatelessWidget {
-  const ColumnWidget({
-    super.key,
-    required this.teamName,
-  });
+  const ColumnWidget({super.key, required this.nameTeam});
 
-  final String teamName;
+  final String nameTeam;
 
   @override
   Widget build(BuildContext context) {
+    final cubit=BlocProvider.of<CounterCubit>(context);
+    int score=(nameTeam=="A")?cubit.scoreA:cubit.scoreB;
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       spacing: 7,
       children: [
-        Text("Team $teamName", style: TextStyle(fontSize: 25)),
+        Text("Team $nameTeam", style: TextStyle(fontSize: 25)),
         SizedBox(height: 15),
-        Text(
-          "0",
-          style: TextStyle(fontSize: 35, fontWeight: FontWeight.w700),
-        ),
+        Text("$score", style: TextStyle(fontSize: 35, fontWeight: FontWeight.w700)),
         SizedBox(height: 15),
-        ButtonWidget(title: "Add 1 point", onPressed:(){}),
+        ButtonWidget(title: "Add 1 point", onPressed: () {cubit.increaseScore(nameTeam: nameTeam, point: 1);}),
         SizedBox(height: 5),
-        ButtonWidget(title: "Add 2 point", onPressed: (){}),
+        ButtonWidget(title: "Add 2 point", onPressed: () {cubit.increaseScore(nameTeam: nameTeam, point: 2);}),
         SizedBox(height: 5),
-        ButtonWidget(title: "Add 3 point", onPressed: (){}),
+        ButtonWidget(title: "Add 3 point", onPressed: () {cubit.increaseScore(nameTeam: nameTeam, point: 3);}),
         SizedBox(height: 5),
       ],
     );
